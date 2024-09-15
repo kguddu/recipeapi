c64546c851b384664134388e0dfa3f49d63cfd8c
guddu <kguddu.1011@gmail.com>
2024-09-15 21:58:08 +0530

    prod file changed

--- src/main/java/com/RecipeSelectionTest/RecipeSelectionApi/s/Application.java
+++ src/main/java/com/RecipeSelectionTest/RecipeSelectionApi/s/Application.java
@@ -1,13 +1,13 @@
-package com.recipewb.recipe;
+package com.RecipeSelectionTest.RecipeSelectionApi.s;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class RecipeApplication {
+public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(RecipeApplication.class, args);
+		SpringApplication.run(Application.class, args);
 	}
 
 }
--- src/test/java/com/RecipeSelectionTest/RecipeSelectionApi/s/ApplicationTests.java
+++ src/test/java/com/RecipeSelectionTest/RecipeSelectionApi/s/ApplicationTests.java
@@ -1,10 +1,10 @@
-package com.recipewb.recipe;
+package com.RecipeSelectionTest.RecipeSelectionApi.s;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class RecipeApplicationTests {
+class ApplicationTests {
 
 	@Test
 	void contextLoads() {
